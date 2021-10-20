30e1a8f3e8da4e7740865567cd5d032829085d80
tweltixx <ilyasmirnov20060506@gmail.com>
2021-10-20 23:55:36 +0300

    React & TypeScript
    React & TypeScript

---
+++ precache-manifest.b83574eb999763a9b566299313b9d4ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "66000ca7297a05b826a4",
+    "url": "/react-build-finest-game-deploy/static/css/main.f7d9bf24.chunk.css"
+  },
+  {
+    "revision": "66000ca7297a05b826a4",
+    "url": "/react-build-finest-game-deploy/static/js/main.8d85c272.chunk.js"
+  },
+  {
+    "revision": "98e964fd9a5ba78fea01",
+    "url": "/react-build-finest-game-deploy/static/js/runtime~main.e8f8311a.js"
+  },
+  {
+    "revision": "e35cc18d2c5435ad7514",
+    "url": "/react-build-finest-game-deploy/static/css/2.98c15a95.chunk.css"
+  },
+  {
+    "revision": "e35cc18d2c5435ad7514",
+    "url": "/react-build-finest-game-deploy/static/js/2.8a85b44f.chunk.js"
+  },
+  {
+    "revision": "27ce04fde27094f5fa8ab839f9546b50",
+    "url": "/react-build-finest-game-deploy/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-build-finest-game-deploy/precache-manifest.b83574eb999763a9b566299313b9d4ec.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-build-finest-game-deploy/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
